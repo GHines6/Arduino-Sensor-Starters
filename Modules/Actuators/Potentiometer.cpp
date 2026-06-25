90120b0315f7d428412f51a320270c597ecb5e0a
Giovanni Hines <gfhines07@outlook.com>
2026-06-25 18:46:25 -0400

    Read potentiometer value in loop
    Added potentiometer reading in the loop function.

---
+++ Modules/Actuators/Potentiometer.cpp
@@ -0,0 +1,12 @@
+#define POT_PIN = 34 // Pin number
+
+void setup() {
+
+}
+
+void loop() {
+  
+ int POTVal = analogRead(POT_PIN);  // Reads the value from potentiometer and stores POTVal
+ delay(1000); // Delays the code for 1 second
+
+}
